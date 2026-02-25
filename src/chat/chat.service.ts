afb89b4f889184734b6c5b40035757150d181e92
shemil076 <shemil076@gmail.com>
2026-02-25 15:17:27 +0530

    updated stream function cater the payload

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -26,10 +26,14 @@ export class ChatService {
   ) {
     const userMessage: Message = {
       id: uuidv4(),
-      role: 'user',
-      content: message,
+      role: 'USER',
+      type: 'TEXT',
+      content: null,
       createdAt: new Date(),
       conversationId: conversationId,
+      metadata: {
+        content: message,
+      },
     };
 
     // Saving the user's message
@@ -37,8 +41,10 @@ export class ChatService {
 
     const history = await this.redisService.getMessages(conversationId);
 
-     const historyArray = history.map(({content, role}) => ({role, content}));
-
+    const historyArray = history.map(({ metadata, role }) => ({
+      role,
+      content: (metadata as { content: string })?.content ?? '',
+    }));
 
     const response = await fetch(
       'http://host.docker.internal:8000/chat/stream',
@@ -55,41 +61,161 @@ export class ChatService {
     const reader = response.body.getReader();
     const decoder = new TextDecoder();
     let assistantFullText: string[] = [];
+    // let isChucksTypeText: boolean[] = [];
+    let parsedItemsArray: any[] = [];
+
+    // while (true) {
+    //   const { done, value } = await reader.read();
+
+    //   if (done) break;
+
+    //   const chunk = decoder.decode(value);
+
+    //   if (chunk === '[DONE]') continue;
+
+    //   try {
+    //     const jsonString = chunk.replace(/^data:\s*/, '');
+    //     const parsed = JSON.parse(jsonString);
+
+    //     const { event, type, payload } = parsed;
+
+    //     parsedItemsArray.push(parsed);
+
+    //     isChucksTypeText.push(type === 'TEXT');
+    //     if (type === 'TEXT') {
+    //       assistantFullText.push(payload.content);
+    //     }
+
+    //     res.write(chunk);
+    //   } catch (e) {
+    //     console.warn('Skipping invalid JSON:', chunk);
+    //   }
+    // }
+
+    // if (assistantFullText.length > 2) {
+    //   if (isChucksTypeText.every((item) => item == true)) {
+    //     const cleanedText = assistantFullText
+    //       .map((word) => word.trim().replace(/,$/, ''))
+    //       .join(' ');
+
+    //     // Saving the assistant's message
+    //     await this.redisService.saveNewMessage(conversationId, {
+    //       id: uuidv4(),
+    //       role: 'ASSISTANT',
+    //       content: null,
+    //       createdAt: new Date(),
+    //       conversationId,
+    //       metadata: {
+    //         content: cleanedText,
+    //       },
+    //       type: 'TEXT',
+    //     });
+    //   }
+    // } else {
+    //   parsedItemsArray.map(async ({ type, payload }) => {
+    //     if (type === 'TEXT') {
+    //       await this.redisService.saveNewMessage(conversationId, {
+    //         id: uuidv4(),
+    //         role: 'ASSISTANT',
+    //         content: null,
+    //         createdAt: new Date(),
+    //         conversationId,
+    //         metadata: {
+    //           content: payload.content,
+    //         },
+    //         type: 'TEXT',
+    //       });
+    //     } else {
+    //       await this.redisService.saveNewMessage(conversationId, {
+    //         id: uuidv4(),
+    //         role: 'ASSISTANT',
+    //         content: null,
+    //         createdAt: new Date(),
+    //         conversationId,
+    //         metadata: {
+    //           chips: payload.chips,
+    //           node_id: payload.node_id,
+    //         },
+    //         type: 'CHIP_RESPONSE',
+    //       });
+    //     }
+    //   });
+    // }
+
+    // const cleanedText = assistantFullText
+    //   .map((word) => word.trim().replace(/,$/, ''))
+    //   .join(' ');
+
+    // // Saving the assistant's message
+    // await this.redisService.saveNewMessage(conversationId, {
+    //   id: uuidv4(),
+    //   role: 'ASSISTANT',
+    //   content: null,
+    //   createdAt: new Date(),
+    //   conversationId,
+    //   metadata: null,
+    //   type: 'TEXT',
+    // });
 
     while (true) {
       const { done, value } = await reader.read();
-
       if (done) break;
 
       const chunk = decoder.decode(value);
+      const event = chunk.split(/\n\n/);
 
-      if (chunk === '[DONE]') continue;
+      for (const eventString of event) {
+        if (!eventString.trim()) continue;
 
-      try {
-        const jsonString = chunk.replace(/^data:\s*/, '');
-        const parsed = JSON.parse(jsonString);
+        const jsonString = eventString.replace(/^data:\s*/, '').trim();
+        if (jsonString === '[DONE]') continue;
 
-        assistantFullText.push(parsed.content);
+        try {
+          const parsed = JSON.parse(jsonString);
+          const { type, payload } = parsed;
 
-        res.write(chunk);
-      } catch (e) {
-        console.warn('Skipping invalid JSON:', chunk);
+          parsedItemsArray.push(parsed);
+
+          if (type === 'TEXT') {
+            assistantFullText.push(payload.content);
+          }
+          res.write(eventString + '\n\n');
+        } catch (e) {
+          console.warn('Skipping invalid JSON:', eventString);
+        }
       }
     }
 
+    for (const item of parsedItemsArray) {
+      if (item.type == 'CHIP_RESPONSE') {
+        await this.redisService.saveNewMessage(conversationId, {
+          id: uuidv4(),
+          role: 'ASSISTANT',
+          content: null,
+          createdAt: new Date(),
+          conversationId,
+          metadata: {
+            chips: item.payload.chips,
+            node_id: item.payload.node_id,
+          },
+          type: 'CHIP_RESPONSE',
+        });
+      }
+    }
 
-    const cleanedText = assistantFullText
-      .map((word) => word.trim().replace(/,$/, ''))
-      .join(' ');
+    if(assistantFullText.length > 0) {
+      const cleanedText = assistantFullText.map(word => word.trim().replace(/,$/, '')).join(' ')
 
-    // Saving the assistant's message
-    await this.redisService.saveNewMessage(conversationId, {
-      id: uuidv4(),
-      role: 'assistant',
-      content: cleanedText,
+      await this.redisService.saveNewMessage(conversationId, {
+        id: uuidv4(),
+      role: 'ASSISTANT',
+      content: null,
       createdAt: new Date(),
       conversationId,
-    });
+      metadata: { content: cleanedText },
+      type: 'TEXT',
+      })
+    }
 
     return response.body;
   }
